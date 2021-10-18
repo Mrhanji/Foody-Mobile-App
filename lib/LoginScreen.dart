4ba65d4912c51d5df87955ff3b1a3f36d9dbd7f2
Sagar Sharma <sagarsharma995899@gmail.com>
2021-10-18 20:39:21 +0530

    v1

--- lib/LoginScreen.dart
+++ lib/LoginScreen.dart
@@ -11,13 +11,32 @@ class LoginScreen extends StatefulWidget {
 class _LoginScreenState extends State<LoginScreen> {
   @override
   Widget build(BuildContext context) {
+    var url = "https://foody.partibha.ml/login.php";
     var size = MediaQuery.of(context).size;
+    final _k = new FlutterWebviewPlugin();
     return MaterialApp(
       debugShowCheckedModeBanner: false,
       home: Scaffold(
-        appBar: AppBar(title: Text('Foody'),backgroundColor: Colors.redAccent,centerTitle: true,),
-        body: WebviewScaffold(url: 'https://foody.partibha.ml/login.php')
-      ),
+          body: WebviewScaffold(
+        url: url,
+        appCacheEnabled: true,
+        appBar: AppBar(
+          title: Text('Foody'),
+          backgroundColor: Colors.redAccent,
+          centerTitle: true,
+          leading: IconButton(
+            icon: Icon(Icons.home),
+            onPressed: () {
+              setState(() {
+                url = 'https://foody.partibha.ml';
+                _k.dispose();
+                _k.close();
+                _k.launch(url);
+              });
+            },
+          ),
+        ),
+      )),
     );
   }
 }
